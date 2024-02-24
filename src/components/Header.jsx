66ea5ad8e2877ec69f5bbff73a5ac9d4d011febf
Siddharth <siddharth122002@gmail.com>
2024-02-24 14:40:31 +0530

    updated home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import {HStack,Button} from '@chakra-ui/react'
 import {Link} from 'react-router-dom'
 export default function Header(){
     return(
-        <HStack  p={4} shadow={'base'} bgColor={'blackAlpha.900'}>
+        <HStack  p={4} shadow={'base'} bgColor={'black'}>
             <Button px={5} variant={'unstyled'} color={'white'}>
                 <Link to={'/'}>Home</Link>
             </Button>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,25 @@
-import image from '../assets/thought-catalog-I0TDRP0fj6Y-unsplash.jpg'
-import '../styles/Home.css'
-export default function Home(){
-    return(
-        <div id='outer'>
-            <img src={image}/>
-            <div id='inner'>
-                <h1>CRYPTO</h1>
-            </div>
-        </div>
-    )
+import image from '../assets/thought-catalog-I0TDRP0fj6Y-unsplash.jpg';
+import { Box, Image,Text } from '@chakra-ui/react';
+
+export default function Home() {
+    return (
+        <Box  bgColor={"black"} w={"full"} h={"100vh"}>
+      <Text
+        fontSize={"5xl"}
+        textAlign={"center"}
+        fontWeight={"bold"}
+        color={"white"}
+        letterSpacing={3}
+        filter={"blur(10px"}
+      >
+        CRYPTO
+      </Text>
+            <Image
+          w={"full"}
+          h={"80vh"}
+          objectFit={"contain"}
+          src={image}
+        />
+        </Box>
+    );
 }
